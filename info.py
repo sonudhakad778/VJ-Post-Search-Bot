e26b26d0bc3d94c6cf7a8f2f80bf487ce455bd53
sonudhakad778 <145945975+sonudhakad778@users.noreply.github.com>
2024-10-30 10:28:08 +0530

    Update info.py

--- info.py
+++ info.py
@@ -9,8 +9,8 @@ from os import environ
 
 API_ID       = int(environ.get("API_ID", "27696177"))
 API_HASH     = environ.get("API_HASH", "0c44906a4feff3b947db76dfa7c57d88")
-BOT_TOKEN    = environ.get("BOT_TOKEN", "")
-DATABASE_URI = environ.get("DATABASE_URI", "")
+BOT_TOKEN    = environ.get("BOT_TOKEN", "5918038913:AAEJs4WeXqlDJ7NQmv698oOD-9RZ9L2Hcjc")
+DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://Spiderman:ohWHjNXG9VedoC5R@cluster0.uuauf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 LOG_CHANNEL  = int(environ.get("LOG_CHANNEL", "-1002116984433"))
 ADMIN        = int(environ.get("ADMIN", "852190589"))
 CHANNEL      = environ.get("CHANNEL", "@super4crazy")
